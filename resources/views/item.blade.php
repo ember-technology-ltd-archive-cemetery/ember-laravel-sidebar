a777f311633ded494d44b5e8abf49e2bd1fbb7da
Dwain Maxwell <dwain@ember.ltd>
2018-06-07 12:25:01 +0100

    Support Material Icons instead

--- resources/views/item.blade.php
+++ resources/views/item.blade.php
@@ -1,13 +1,15 @@
 <li class="@if($item->getItemClass()){{ $item->getItemClass() }}@endif @if($active)active @endif @if($item->hasItems())treeview @endif clearfix">
     <a href="{{ $item->getUrl() }}" class="@if(count($appends) > 0) hasAppend @endif" @if($item->getNewTab())target="_blank"@endif>
-        <i class="{{ $item->getIcon() }}"></i>
+        <i class="md-18 material-icons">{{ $item->getIcon() }}</i>
+        <!-- <i class="{{ $item->getIcon() }}"></i> -->
+
         <span>{{ $item->getName() }}</span>
 
         @foreach($badges as $badge)
             {!! $badge !!}
         @endforeach
 
-        @if($item->hasItems())<i class="{{ $item->getToggleIcon() }} pull-right"></i>@endif
+        <!-- @if($item->hasItems())<i class="{{ $item->getToggleIcon() }} pull-right"></i>@endif -->
     </a>
 
     @foreach($appends as $append)
